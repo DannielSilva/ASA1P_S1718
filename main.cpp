49cee6c036de263824a9163a1974fa7649f01012
Joao Daniel Rua Ferreira Pires da Silva <joaodaniel1998@gmail.com>
2018-03-14 15:00:20 +0000

    ja temos uma lista de adjacencias que se imprime :o

--- main.cpp
+++ main.cpp
@@ -2,28 +2,30 @@
 #include <list>
 using namespace std;
 
-class Grafo {
-    int _size;
+typedef struct grafoj {
+  int numV;
 
-  public:
-    Grafo(int size) {
-
-    }
-    void printname() {
-    }
-};
+} *Grafo;
 
 int main(int argc, char const *argv[]) {
   int pontos;
   int ligacoes;
   int v1, v2, i;
-  //Grafo grafo;
+  list<int>* grafo; //ISTO E UMN GRAFO :O
 
   scanf("%d\n%d", &pontos, &ligacoes);
-  //printf("%d %d\n", pontos, ligacoes);
+  grafo = new list<int>[pontos+1];
+
   for(i=0; i<ligacoes; i++) {
     scanf("%d %d", &v1, &v2);
+    grafo[v1].push_back(v2);
+  }
 
+  list<int>::iterator it;
+  for(i=1; i<=pontos; i++) {
+    for (it = grafo[i].begin(); it != grafo[i].end(); ++it) {
+      printf("%d %d\n", i, *it);
+    }
   }
 
   return 0;
