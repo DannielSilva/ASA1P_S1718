b122b22cab8f8ba4aacc3e7ee82dc44fb1958548
Francisco Sousa <f.sou@hotmail.com>
2018-03-17 19:27:11 +0000

    Comentado tudo para começar de novo

--- main.cpp
+++ main.cpp
@@ -3,83 +3,84 @@
 #include <stack>
 using namespace std;
 
-#define min(A,B) A<B ? A : B
+#define min(A, B) A < B ? A : B
 
-typedef struct vertice {
+/********************************** ESTRUTURAS **********************************/
+/*typedef struct vertice {
   int discovery = 0;
   int low;
   int stack = 0;
   list<int> adjacentes;
 } Vertice;
 
-typedef Vertice* Grafo;
+typedef Vertice* Grafo;*/
 
 typedef struct args_struct {
-	Grafo* g;
-	int visited, pontos;
-	stack<int>* stackV;
-}* Args_p;
+  //Grafo* g;
+  int visited, pontos;
+  stack<int>* stackV;
+} * Args_p;
 
+/********************************** TARJAN **********************************/
 void visit_Tarjan(int curV, Args_p args) {
-	list<int>::iterator itv;
-	int poppedV;
+  /*list<int>::iterator itv;
+  int poppedV;
 
-	args->g[curV]->discovery = args->g[curV]->low = args->visited++;
-	args->stackV->push(curV);
-	args->g[curV]->stack = 1;
-	for (itv = args->g[curV]->adjacentes.begin(); itv != args->g[curV]->adjacentes.end(); ++itv) {
-		if (!args->g[*itv]->discovery || args->g[*itv]->stack) {
-			if (!args->g[*itv]->discovery) {
-				visit_Tarjan(*itv, args);
-			}
-			args->g[curV]->low = min(args->g[curV]->low, args->g[*itv]->low);
-		}
-	}
+  args->g[curV]->discovery = args->g[curV]->low = args->visited++;
+  args->stackV->push(curV);
+  args->g[curV]->stack = 1;
+  for (itv = args->g[curV]->adjacentes.begin();
+       itv != args->g[curV]->adjacentes.end(); ++itv) {
+    if (!args->g[*itv]->discovery || args->g[*itv]->stack) {
+      if (!args->g[*itv]->discovery) {
+        visit_Tarjan(*itv, args);
+      }
+      args->g[curV]->low = min(args->g[curV]->low, args->g[*itv]->low);
+    }
+  }
 
-	if (args->g[curV]->discovery == args->g[curV]->low) {
-		poppedV = args->stackV->top();
-		args->stackV->pop();
-		args->g[curV]->stack = 1;
-	}
+  if (args->g[curV]->discovery == args->g[curV]->low) {
+    poppedV = args->stackV->top();
+    args->stackV->pop();
+    args->g[curV]->stack = 1;
+  }*/
 }
-
 
 void scc_Tarjan(Args_p args) {
-	for (int i = 1; i < args->pontos; i++) {
-		if (!args->g[i]->discovery) {
-			visit_Tarjan(i, args);
-		}
-	}
+  /*for (int i = 1; i < args->pontos; i++) {
+    if (!args->g[i]->discovery) {
+      visit_Tarjan(i, args);
+    }
+  }*/
 }
 
-
-int main(int argc, char const *argv[]) {
-  Grafo grafo; //ISTO E UMN GRAFO :O
+/********************************** MAIN **********************************/
+int main(int argc, char const* argv[]) {
+  // Grafo grafo;
   int pontos, ligacoes, vPai, vFilho, i;
-  list<int>::iterator it;
+  //list<int>::iterator it;
 
   scanf("%d\n%d", &pontos, &ligacoes);
 
-  grafo = new Vertice[pontos+1];
+  //grafo = new Vertice[pontos + 1];
   for (i = 0; i < ligacoes; i++) {
     scanf("%d %d", &vPai, &vFilho);
-    grafo[vPai].adjacentes.push_front(vFilho);
+    //grafo[vPai].adjacentes.push_front(vFilho);
   }
 
-  Args_p args = new args_struct;
+  /*Args_p args = new args_struct;
   args->g = &grafo;
   args->visited = 0;
   args->pontos = pontos;
-  args->stackV = new stack<int>;
-
+  args->stackV = new stack<int>;*/
 
   for (i = 1; i <= pontos; i++) {
-    for (it = grafo[i].adjacentes.begin(); it != grafo[i].adjacentes.end(); ++it) {
-		printf("%d %d\n", i, *it);
-	}
+    /*for (it = grafo[i].adjacentes.begin(); it != grafo[i].adjacentes.end(); ++it) {
+				printf("%d %d\n", i, *it);
+			}*/
   }
 
-	scc_Tarjan(args);
+  //scc_Tarjan(args);
 
   return 0;
 }
